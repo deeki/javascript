4c4d19b5a32f7c9f4b0264f4c766ed5b7a284047
deeki <deekshagupta0207@gmail.com>
2020-05-30 23:03:08 +0530

    Create fizzbuzz.js

---
+++ fizzbuzz.js
@@ -0,0 +1,19 @@
+function solution(number) {
+  if (!number) {
+        return false;
+  }
+  for(var i = 1; i < number; i++) {
+        if(i % 3 === 0 && i % 5 === 0){
+            console.log("FizzBuzz");
+        }
+        else if(i % 5 === 0) {
+            console.log("Buzz");
+        }
+        else if(i % 3 === 0){
+            console.log("Fizz");
+        }
+        else {
+            console.log(i);
+        }
+  }
+}
